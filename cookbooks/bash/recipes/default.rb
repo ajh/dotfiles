328fad71c68c3cc464746937f3c793c272d967e9
vagrant <vagrant@lucid32.(none)>
2012-04-02 10:32:08 -0700

    refactor and move dotfiles

--- cookbooks/bash/recipes/default.rb
+++ cookbooks/bash/recipes/default.rb
@@ -15,6 +15,22 @@ CONFIG = Config.new.tap {|c|
 }
 
 class DotFile
+  # Returns a list of dot files for the given cookbook. Dotfiles are found in the 'files' folder of the cookbook and automatically translated into
+  def self.find(cookbook_name)
+    paths = []
+
+    Find.find CONFIG.root_path.join('cookbooks', cookbook_name, 'dotfiles') do |path|
+      path = Pathname.new path
+
+      path.file? or next
+      path.basename.to_s[0] != '.' or next # ignore real dotfiles
+
+      paths << DotFile.new(path)
+    end
+
+    paths
+  end
+
   def initialize(cookbook_path)
     @source = Pathname.new cookbook_path
   end
@@ -25,34 +41,14 @@ class DotFile
   # The path for where the file should be installed. Todo, remove reference to ENV['HOME'] here.
   def target
     target = @source.to_s \
-      .sub(%r"#{Regexp.escape CONFIG.root_path.to_s}/cookbooks/[^/]+/files", CONFIG.install_path.to_s) \
+      .sub(%r"#{Regexp.escape CONFIG.root_path.to_s}/cookbooks/[^/]+/dotfiles", CONFIG.install_path.to_s) \
       .gsub(%r/(\W|^)dot_(\w+)/, '\1.\2')
 
     Pathname.new target
   end
 end
 
-# Returns a list of dot files for the given cookbook. Dotfiles are found in the 'files' folder of the cookbook and automatically translated into
-def find_dot_files(cookbook_name)
-  paths = []
-
-  Find.find CONFIG.root_path.join('cookbooks', cookbook_name, 'files') do |path|
-    path = Pathname.new path
-
-    path.file? or next
-    path.basename.to_s[0] != '.' or next # ignore real dotfiles
-
-    paths << DotFile.new(path)
-  end
-
-  paths
-end
-
-find_dot_files('bash').each do |dotfile|
-  puts [dotfile.source.to_s, dotfile.target.to_s].inspect
+DotFile.find('bash').each do |dotfile|
   directory dotfile.target.dirname.to_s
-
-  link dotfile.target.to_s do
-    to dotfile.source.to_s
-  end
+  link(dotfile.target.to_s) { to dotfile.source.to_s }
 end
