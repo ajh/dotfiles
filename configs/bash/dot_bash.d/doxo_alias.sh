f20156bdbcf4365b7de56947e87e1cd4cad588db
Andy Hartford <hartforda@gmail.com>
2012-06-12 19:18:44 -0700

    Fixes to doxo aliases

--- configs/bash/dot_bash.d/doxo_alias.sh
+++ configs/bash/dot_bash.d/doxo_alias.sh
@@ -8,5 +8,5 @@ alias saba="cd -P ~/devel/saba"
 alias snowbell="cd -P ~/devel/snowbell"
 
 # aspen
-alias aspen-rebuild="rails-rebuild && echo 'bundle exec rake ts:rebuild bin:apply_sample_documents bin:apply_sample_logos_to_providers' | sh -x"
-alias aspen-restart="rails-restart"
+alias aspen-rebuild="rails-rebuild && aspen-restart && echo 'bundle exec rake ts:rebuild bin:apply_sample_documents bin:apply_sample_logos_to_providers' | sh -x"
+alias aspen-restart="rails-restart && script/delayed_job stop && script/delayed_job start"
