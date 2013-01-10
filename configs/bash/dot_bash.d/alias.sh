5d0c20c239dc52dc088263eb45e2540436f9a2ad
Andrew Hartford <andrew.hartford@doxo.com>
2013-01-10 15:20:06 -0500

    Some bash config changes.
    Including the nice be alias. Also avoid db:seed since it crashes in
    aspen these days.

--- configs/bash/dot_bash.d/alias.sh
+++ configs/bash/dot_bash.d/alias.sh
@@ -6,5 +6,5 @@ alias ls='ls -G'
 
 # rails
 alias rails-grep="grep --exclude-dir='public/system' --exclude-dir='tmp' --exclude='tags' --exclude='*.log'"
-alias rails-rebuild="echo 'bundle exec rake db:reset db:seed db:fixtures:load FIXTURES_PATH=spec/fixtures log:clear && rm -rf public/system/* && bundle exec rake db:test:prepare' | sh -x"
+alias rails-rebuild="echo 'bundle exec rake db:drop db:create db:schema:load db:fixtures:load FIXTURES_PATH=spec/fixtures log:clear && rm -rf public/system/* && bundle exec rake db:test:prepare' | sh -x"
 alias rails-restart="touch tmp/restart.txt"
---
+++ configs/bash/dot_bash.d/editor.sh
@@ -0,0 +1,3 @@
+export EDITOR=vim
+export VISUAL=vim
+export GIT_EDITOR=vim
