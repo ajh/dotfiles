3b203da7dcf09188143589169523cc138cc157e2
Andy Hartford <hartforda@gmail.com>
2013-01-29 18:56:31 -0500

    Add bundler alias

--- configs/bash/dot_bash.d/doxo_alias.sh
+++ configs/bash/dot_bash.d/doxo_alias.sh
@@ -1,3 +1,5 @@
 # aspen
 alias aspen-rebuild="rails-rebuild && aspen-restart && echo 'bundle exec rake ts:rebuild bin:apply_sample_documents bin:apply_sample_logos_to_providers' | sh -x"
 alias aspen-restart="rails-restart && script/delayed_job stop && script/delayed_job start"
+
+alias be="bundle exec"
