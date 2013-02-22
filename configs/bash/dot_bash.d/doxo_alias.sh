ff7b29241c7650e576075ead8e357bc9f673d53e
Andrew Hartford <andrew.hartford@doxo.com>
2013-02-22 08:54:05 -0500

    Add bash variable for app paths

--- configs/bash/dot_bash.d/doxo_alias.sh
+++ configs/bash/dot_bash.d/doxo_alias.sh
@@ -3,3 +3,6 @@ alias aspen-rebuild="rails-rebuild && aspen-restart && echo 'bundle exec rake ts
 alias aspen-restart="rails-restart && script/delayed_job stop && script/delayed_job start"
 
 alias be="bundle exec"
+
+# Can be used like: for i in $APP_PATHS; do echo $i done
+APP_PATHS="/home/ajh/devel/aspen /home/ajh/devel/balsa /home/ajh/devel/cacao /home/ajh/devel/doha /home/ajh/devel/poui /home/ajh/devel/reverse_proxy /home/ajh/devel/saba /home/ajh/devel/snowbell"
