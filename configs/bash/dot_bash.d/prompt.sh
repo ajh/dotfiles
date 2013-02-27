a735d1c94e921a89670c4789ddf18700815d41ac
Andrew Hartford <andrew.hartford@doxo.com>
2013-02-26 20:12:59 -0500

    Add Philip's custom prompt which is cool

---
+++ configs/bash/dot_bash.d/prompt.sh
@@ -0,0 +1,17 @@
+function ps1_git {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo " … "${ref#refs/heads/}
+}
+
+ps1="\[\e[0;32m\]"                         # colored text
+ps1=$ps1"\u@\h"                            # user@host
+ps1=$ps1" … "
+ps1=$ps1"\$(rvm-prompt v)\$(rvm-prompt g)" # 1.9.3@gemset
+ps1=$ps1"\$(ps1_git)"                      # git branch
+ps1=$ps1" … "
+ps1=$ps1"\w"                               # relative working dir
+ps1=$ps1"\\n"                              # new line
+ps1=$ps1"\$"                               # $
+ps1=$ps1" "                                # space
+ps1=$ps1"\[\e[0m\]"                        # reset color
+export PS1=$ps1
