020ee1160e78b2ad522e763436a27c68e58ede76
Andrew Hartford <andrew.hartford@doxo.com>
2013-03-01 12:07:59 -0500

    tweak prompt a little more

--- configs/bash/dot_bash.d/prompt.sh
+++ configs/bash/dot_bash.d/prompt.sh
@@ -3,15 +3,18 @@ function ps1_git {
   echo " … "${ref#refs/heads/}
 }
 
-ps1="\[\e[0;32m\]"                         # colored text
-ps1=$ps1"\h"                               # hostname
-ps1=$ps1" … "
-ps1=$ps1"\$(rvm-prompt i v)\$(rvm-prompt g)" # 1.9.3@gemset
-ps1=$ps1"\$(ps1_git)"                      # git branch
-ps1=$ps1" … "
-ps1=$ps1"\W"                               # relative working dir
-ps1=$ps1"\\n"                              # new line
-ps1=$ps1"\$"                               # $
-ps1=$ps1" "                                # space
-ps1=$ps1"\[\e[0m\]"                        # reset color
+function ps1_rvm {
+  value=$(rvm-prompt i v g) || return # 1.9.3-p303@global
+  echo " … "${value}
+}
+
+ps1="\[\e[0;32m\]"      # colored text
+ps1=$ps1"\h"            # hostname
+ps1=$ps1"\$(ps1_rvm)"   # 1.9.3@gemset
+ps1=$ps1"\$(ps1_git)"   # git branch
+ps1=$ps1" … \W"         # relative working dir
+ps1=$ps1"\\n"           # new line
+ps1=$ps1"\$"            # $
+ps1=$ps1" "             # space
+ps1=$ps1"\[\e[0m\]"     # reset color
 export PS1=$ps1
