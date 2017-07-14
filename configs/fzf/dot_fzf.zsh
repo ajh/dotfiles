751929e0a7b5dcc5d85cb7f6aea0146b87942837
Andy Hartford <ajh@amazon.com>
2017-07-14 11:40:42 -0400

    Add fzf config

---
+++ configs/fzf/dot_fzf.zsh
@@ -0,0 +1,30 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/ajh/.fzf/bin* ]]; then
+  export PATH="$PATH:/Users/ajh/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/ajh/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/ajh/.fzf/shell/key-bindings.zsh"
+
+function cdfzf() {
+  # if no args, use '.'
+  if (( $# == 0 )); then
+    starting_paths="."
+  else
+    starting_paths=$argv
+  fi
+
+  chosen_path=$(find $starting_paths -type d -not -path '*/\.*' | fzf --height=20% --border --color)
+
+  if [ $? -eq 0 ]; then
+    cd $chosen_path
+  else
+    false
+  fi
+}
